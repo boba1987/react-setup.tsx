437767eba6f3cc8fb10776945a98c6fcc9721235
boba1987 <boba_dj1987@hotmail.com>
2020-02-09 11:52:30 +0100

    button event handler

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,10 +2,11 @@ import React, {ReactNode} from "react";
 import styles from './button.module.scss';
 
 interface ButtonInterface {
-    children?: ReactNode
+    children?: ReactNode,
+    onclick?: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 const Button = (props: ButtonInterface) => <>
-    <button className={styles.button}>{ props.children }</button>
+    <button className={styles.button} onClick={props.onclick}>{ props.children }</button>
 </>
 
 export default Button;
